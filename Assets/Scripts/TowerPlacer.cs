27bfb8aa39385a5bd10579c4a81f40447de41709
gergo0505 <kanyari.gergo@gmail.com>
2025-04-13 15:42:24 +0200

    gg

--- Assets/Scripts/TowerPlacer.cs
+++ Assets/Scripts/TowerPlacer.cs
@@ -3,8 +3,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
-
 
 public class TowerPlacer : MonoBehaviour
 {
